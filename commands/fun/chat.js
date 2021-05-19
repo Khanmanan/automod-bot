5fac9353b5f8ad15d5082137911754d5050e4b33
Khanmanan <65243374+Khanmanan@users.noreply.github.com>
2021-05-20 01:16:46 +0530

    Delete commands/fun directory

--- commands/fun/chat.js
+++
@@ -1,9 +0,0 @@
-const { chatBot } = require('reconlx') 
-
-module.exports = {
-    name : 'c',
-    category: 'fun',
-    run : async(client, message, args) => {
-        chatBot(message, args.join(" "))
-    }
-}
