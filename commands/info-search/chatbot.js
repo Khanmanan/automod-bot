e12b1887ad2710570e289c824fa7506fd982d954
Khanmanan <65243374+Khanmanan@users.noreply.github.com>
2021-05-20 01:18:09 +0530

    Delete commands/info-search directory

--- commands/info-search/chatbot.js
+++
@@ -1,9 +0,0 @@
-const msg = require("discord.js");
-module.exports = {
-  name: "chatbot",
-  category: "info",
-  description: "Returns latency and API ping",
-  run: async (client, message, args) => {
-    
-  }
-  }
