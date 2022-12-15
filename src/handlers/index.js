c0fc69b3d69af0ab600326e4c87f7081b24826aa
Cwkhan <65243374+Khanmanan@users.noreply.github.com>
2022-12-16 01:16:55 +0530

    Delete src directory

--- src/handlers/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  automodHandler: require("./automod"),
-  counterHandler: require("./counter"),
-  greetingHandler: require("./greeting"),
-  inviteHandler: require("./invite"),
-  reactionHandler: require("./reaction"),
-  xpHandler: require("./xp"),
-};
